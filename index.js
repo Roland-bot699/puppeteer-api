f4a6885a2cb12d3c8eb6893290e2fb30ceb62e97
許佑全 <xuyouquan@xuyouquandeMacBook-Air.local>
2025-07-12 06:15:56 +0800

    fix: correct lock file and dependency versions

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${3000}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
